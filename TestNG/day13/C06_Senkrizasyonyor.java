b1ece75b4c95efe46abf8325338b6cd1f05ece06
ilknurturksever <ilknurturksever1819@gmail.com>
2022-04-02 23:24:06 +0300

    subject

---
+++ TestNG/day13/C06_Senkrizasyonyor.java
@@ -0,0 +1,31 @@
+package day13;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C06_Senkrizasyonyor extends TestBase {
+    @Test
+    public void test01() throws InterruptedException {
+        /*
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.findElement(By.id("file-submit")).click();
+    */
+        /*
+        driver.get("https://www.youtube.com");
+        driver.findElement(By.xpath("//yt-formatted-string[text()='I Agree']")).click();
+        driver.get("https://www.techproeducation.com");
+        driver.findElement(By.xpath("(//a[text()='info@techproeducation.com'])[2]")).click();
+*/
+        // https://the-internet.herokuapp.com/dynamic_controls sayfasina gidin
+        // disable butonuna basin
+        // disabled yazisinin ciktigini test edin
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        driver.findElement(By.xpath("//button[text()='Enable']")).click();
+        WebElement itsDisabledYazielementi=driver.findElement(By.xpath("//p[text()=\"It's enabled!\"]"));
+        Assert.assertTrue(itsDisabledYazielementi.isDisplayed());
+    }
+
+}
